ee713a23ef42a2f43208ee35bbc98764ff1d0ee1
Rafael Andara <rafael.andara@admios-sa.com>
2022-01-14 22:46:47 -0300

    Updates

---
+++ precache-manifest.77ff8bf354ac607ad37663590151dc91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c364f6e89020e50bd0854cd9cdba8237",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "01b2b60c4cf480bfd696",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.e7bbbfef.chunk.css"
+  },
+  {
+    "revision": "d1bfa1402caf09d0d91e",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.86e2bb01.chunk.js"
+  },
+  {
+    "revision": "01b2b60c4cf480bfd696",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.bebadec2.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
